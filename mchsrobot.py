d60c22435bb6de259f00e09c91cd73c8fb768ce8
PlainOlAir <POL.KevinNguyen@gmail.com>
2018-04-14 14:16:49 -0700

    oof

--- mchsrobot.py
+++ mchsrobot.py
@@ -1,5 +1,7 @@
-left_motor = "47252153627919119984829"
+left_motor = "47260374648549928326927"
 right_motor = "47258887835223006894371"
+leftpull_motor = ""
+rightpull_motor = ""
 
 def autonomous_setup():
     print("Autonomous mode has started!")
@@ -17,17 +19,106 @@ def teleop_setup():
     print("Tele-operated mode has started!")
 
 def teleop_main():
-    def rightjoymove():
-        if abs(Gamepad.get_value("joystick_right_y")) >= 0.1 or abs(Gamepad.get_value("joystick_right_x")) >= 0.1:
-            return 3    
-    def drive(leftmotor, rightmotor):
-        Robot.set_value(left_motor, "duty_cycle", leftmotor)
+    quicc = 1
+    slow = 0.5
+    def lift(speed):
+        Robot.set_value(leftpull_motor, "duty_cycle", speed)
+        Robot.set_value(rightpull_motor, "duty_cycle", -speed)
+    def drive(leftmotor,rightmotor): #drive function
+        Robot.set_value(left_motor, "duty_cycle", (-1 * leftmotor))
         Robot.set_value(right_motor, "duty_cycle", rightmotor)
-    hello = (Gamepad.get_value("joystick_right_y") + Gamepad.get_value("joystick_right_x"))/2
-    bye = (Gamepad.get_value("joystick_right_y") - Gamepad.get_value("joystick_right_x"))/2
-    if(3 == rightjoymove())
-        drive(-1 * bye, hello)
+    
+    if(Gamepad.get_value("button_y") == 1):
+        if(Gamepad.get_value("l_bumper") == 1):
+            lift(slow)
+        else:
+            lift(quicc)
+    elif(Gamepad.get_value("button_a") == 1):
+        if(Gamepad.get_value("l_bumper") == 1):
+            lift(-slow)
+        else:
+            lift(-quicc)
+    
+       
+        #motors will drive at 0.5 due to speed
+    if(Gamepad.get_value("dpad_up") == 1): #dpad up results in robot going forward and so fourth
+        drive(slow,slow)
+    elif(Gamepad.get_value("dpad_down") == 1):
+        drive(-slow,-slow)
+    elif(Gamepad.get_value("dpad_left") == 1):
+        drive(-slow,slow)
+    elif(Gamepad.get_value("dpad_right") == 1):
+        drive(slow,-slow)
     else:
-        Robot.set_value(left_motor, "duty_cycle", 0)
-        Robot.set_value(right_motor, "duty_cycle", 0)
-    print(hello)
+        #these two take half the joystick value and move motors
+        Robot.set_value(left_motor, "duty_cycle", (-slow * Gamepad.get_value("joystick_left_y"))) 
+        Robot.set_value(right_motor, "duty_cycle", (slow * Gamepad.get_value("joystick_right_y")))
+    print("smile") #end line to test if program runs all the way
+    
+def Robot_decode():
+    
+    def next_power(num):
+        count = 0
+        if(num == 0):
+          return 1
+        elif(num & (num - 1) == 0):
+          return num
+        while(num != 0):
+            num >>= 1
+            count += 1
+        return 1 << count
+    
+    def reverse_digits(num):
+        i = 0
+        u = 0
+        while(num > 0):
+          u = num % 10
+          i = (i*10) + u
+          num = num//10
+            
+    def smallest_prime_fact(num):
+      primes = [0] * (num + 1)
+      primes[1] = 1
+      
+      for i in range(2, num + 1):
+        if (primes[i] == 0):
+          primes[i] = i
+          for j in range(2 * i, num + 1, i):
+            if(primes[j] == 0):
+              primes[j] = i
+      print(primes[num])
+      
+    def most_common_digit(num):
+      i = str(num)
+      u = []
+      count_tmp = 0
+      count_list = []
+      count_main = 0
+      for digit in i:
+        u.append(int(digit))
+    
+      for k in range(1, 9):
+        count_tmp = 0
+        for o in range(0, len(u) - 1):
+          if u[o] == k:
+            count_tmp += 1
+        count_list.append(count_tmp)
+        
+      for m in range (1,9):
+        if count_list[m-1] >= count_main:
+          count_main = m
+      print(count_main)
+      
+    def silly_base_two(num):
+      n = str(bin(num))
+      n = n[:0] + n[(2):]
+      print(n)
+      
+    def  
+      
+    
+  
+  
+            
+            
+    
